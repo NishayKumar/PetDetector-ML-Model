cccf2a1b84b0cf42bb7969f87ead70370972523b
Nishaykumar <Nishaykumar24@gmail.com>
2023-08-28 11:12:34 +0530

    Initial Commit

---
+++ PetDetector/PetDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetDetectorApp.swift
+//  PetDetector
+//
+//  Created by Nishay Kumar on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PetDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
